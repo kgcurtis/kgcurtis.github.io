b00f38db85e7b1e85f555ac2e6f962bec8a7e70b
kgcurtis <kcurtis26518@gmail.com>
2019-04-02 11:59:30 -0500

    Update bio

--- src/Bio.js
+++ src/Bio.js
@@ -6,8 +6,7 @@ class Bio extends Component {
   render() {
     return (
       <div className="bio">
-      Hi, I'm Katherine Curtis. I'm a software engineer currently living in Austin, TX but headed to Seattle, WA to work for Microsoft after graduating from UT Austin in Spring 2019.
-      </div>
+Hi, I'm Katherine Curtis. I'm a software engineer currently living in Austin, TX but headed to Seattle, WA to work for Microsoft after graduating from UT Austin in Spring 2019.      </div>
     );
   }
 
